d590aa962f76b3058dff91ba6bb1d6f45126b36f
ImadMachi <machii.imad@gmail.com>
2023-10-09 20:34:00 +0100

    first commit

---
+++ src/components/Icons.tsx
@@ -0,0 +1,12 @@
+import { Moon, Sun, User, Heart, Gift, ShoppingCart } from "lucide-react";
+
+export const Icons = {
+	Sun,
+	Moon,
+	User,
+	Heart,
+	Gift,
+	ShoppingCart,
+};
+
+export default Icons;
